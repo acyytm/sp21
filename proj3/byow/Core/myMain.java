a79031736ceb08be423cff79b34578adfdb0c413
RF <2596811071@qq.com>
2023-01-19 16:19:37 +0800

    remove StdDraw to test

--- proj3/byow/Core/myMain.java
+++ proj3/byow/Core/myMain.java
@@ -8,7 +8,7 @@ public class myMain {
     public static void main(String[] args) {
         Engine e = new Engine();
         TERenderer renderer = new TERenderer();
-        String command = "N1234282S";
+        String command = "n5197880843569031643s";
         TETile[][] world = e.interactWithInputString(command);
         renderer.initialize(80, 30);
         renderer.renderFrame(world);
